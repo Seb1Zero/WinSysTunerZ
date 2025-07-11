1e5ebc04d6a0107233a767d06960f8c7ad2f19b2
Seb1Zero <sebastian.zeisele@gmail.com>
2025-07-11 18:19:13 +0200

    Neues WinSysTunerZ Release 0.1.1+4b3fbde9987e8f4bcd954332a68aa4187e9835c2 - 2025-07-11_18-19

---
+++ WinSysTunerZ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using Velopack;
+
+namespace WinSysTunerZ
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            VelopackApp.Build().Run(); // <- Muss VOR deiner App-Instanz stehen!
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
